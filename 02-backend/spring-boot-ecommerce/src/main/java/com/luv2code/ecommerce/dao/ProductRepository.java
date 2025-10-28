bd1ff7c1a84bb55bdb24ae2aa19731819016893d
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-10-28 17:06:07 -0400

    Add repository interfaces

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.luv2code.ecommerce.dao;
 
-public interface ProductRepository {
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
