a8ba0d98bc0e349e0a5481f1e575bf650ee1c2f4
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-10-29 12:32:46 -0400

    Add product-list component

--- 03-frontend/angular-ecommerce/src/app/app-module.ts
+++ 03-frontend/angular-ecommerce/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ProductList } from './components/product-list/product-list';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    ProductList
   ],
   imports: [
     BrowserModule,
