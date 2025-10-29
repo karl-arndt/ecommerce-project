2a4b534ab61f0553a3bd249eb0f6682e8c7bd9ad
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-10-29 12:43:37 -0400

    Add product-service

--- 03-frontend/angular-ecommerce/src/app/app.ts
+++ 03-frontend/angular-ecommerce/src/app/app.ts
@@ -1,10 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ProductService } from './services/product.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  // imports: [BrowserModule, HttpClientModule],
+  providers: [ProductService]  
 })
 export class App {
   protected readonly title = signal('angular-ecommerce');
